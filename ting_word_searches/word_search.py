fba3dd51a6ed89a667a460738649ef296f669429
NoteVini <vinicius.shk@gmail.com>
2023-04-04 18:44:30 -0300

    feat: Implements search_by_word func to mimc exists_word func with the content alongside it's apearence.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -18,4 +18,19 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = list()
+    ocurrencies = list()
+    info = {
+        "palavra": word,
+        "arquivo": '',
+        }
+    for item in instance.fila:
+        info['arquivo'] = item['nome_do_arquivo']
+        for idx, line in enumerate(item['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                ocurrencies.append({"linha": idx + 1, "conteudo": line})
+        if len(ocurrencies):
+            info['ocorrencias'] = [*ocurrencies]
+            data.append(info)
+            ocurrencies.clear()
+    return data
