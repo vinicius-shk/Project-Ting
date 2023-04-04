60ed45d054fdbd46afdbf5fcb29cfdbf77e0684c
NoteVini <vinicius.shk@gmail.com>
2023-04-04 18:40:54 -0300

    feat: implements the exists_word func to indentify a substr in an str on the queue and return the line its present.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = list()
+    ocurrencies = list()
+    info = {
+        "palavra": word,
+        "arquivo": '',
+        }
+    for item in instance.fila:
+        info['arquivo'] = item['nome_do_arquivo']
+        for idx, line in enumerate(item['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                ocurrencies.append({"linha": idx + 1})
+        if len(ocurrencies):
+            info['ocorrencias'] = [*ocurrencies]
+            data.append(info)
+            ocurrencies.clear()
+    return data
 
 
 def search_by_word(word, instance):
