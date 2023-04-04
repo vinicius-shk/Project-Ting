0604b049870f840893195e40e9d0f3fbe151b158
NoteVini <vinicius.shk@gmail.com>
2023-04-04 16:04:07 -0300

    feat: Implements a process func to manage the txt_importer data and add it to the queue.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in instance.fila:
+        if path_file == item['nome_do_arquivo']:
+            return None
+    process_out = sys.stdout
+    content = txt_importer(path_file)
+    processed_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content,
+    }
+    instance.enqueue(processed_data)
+    print(processed_data, file=process_out)
 
 
 def remove(instance):
