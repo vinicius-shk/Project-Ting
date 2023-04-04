8b9ac548900d381d5b7237a96099bc7236b75ab9
NoteVini <vinicius.shk@gmail.com>
2023-04-04 15:33:02 -0300

    feat: Implements a txt_importer func to gather data from a .txt file and returns a list of its contents

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    txt_stderr = sys.stderr
+    if path_file.split('.')[-1] != 'txt':
+        txt_stderr.write('Formato inválido')
+    try:
+        with open(path_file) as file:
+            content = file.read()
+            return content.split('\n')
+    except FileNotFoundError:
+        txt_stderr.write(f'Arquivo {path_file} não encontrado\n')
