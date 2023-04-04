8606b0db1c173b89f254869c0248570621a7c158
NoteVini <vinicius.shk@gmail.com>
2023-04-04 15:12:02 -0300

    feat: Implements Queue class with enqueue, dequeue and search methods, as well as __len__.

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,18 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.fila = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.fila)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.fila.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.fila.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 > index or index >= len(self.fila):
+            raise IndexError('Índice Inválido ou Inexistente')
+        return self.fila[index]
